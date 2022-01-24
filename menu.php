1aed42f0b82ec6a30ff7813cdb12647b1d036ac9
Umed <umedeminov@list.ru>
2022-01-24 19:39:05 +0500

    Write a comment with ingredients

--- menu.php
+++ menu.php
@@ -1,3 +1,14 @@
 <?php
+/* Pilaf with shrimp 
+500 gram shrimps 
+1,5 cup rice 
+2 onion  
+2 carrot 
+2 cloves of garlic  
+0,5 teaspoons salt
+0,5 teaspoons black pepper, ground
+6 teaspoons vegetable oil
+3 teaspoons soy sauce
+*/
 
 >
